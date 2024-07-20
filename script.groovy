c26326af18d281db8944afc37d364ca22ec511aa
ahmed-mourad1_voda <ahmed.mourad1@vodafone.com>
2024-07-20 14:17:26 +0300

    add groovy script

--- script.groovy
+++ script.groovy
@@ -1,19 +1,14 @@
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
+def buildApp() {
+    echo 'Building the application ...'
+}
 
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
-    }
-} 
+def testAPP() {
+    echo 'Testing the application ...'
+}
 
 def deployApp() {
-    echo 'deploying the application...'
-} 
+    echo 'Deploying the application ...'
+    echo "Deploying version ${params.VERSION}"
+}
 
 return this
