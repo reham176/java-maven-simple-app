b7958beab9a78c3c8305264debe129e02c7c1967
reham176 <59253530+reham176@users.noreply.github.com>
2024-07-31 18:40:01 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -4,7 +4,7 @@ def buildJar() {
 }
 def buildImage() {
     echo 'building the docker image'
-    withcredentials([usernamePassword(credentialsId:'dockerhub-depi', passwordVariable:'PASSWORD' , usernameVariable:'USERNAME')]){
+    withCredentials([usernamePassword(credentialsId:'dockerhub-depi', passwordVariable:'PASSWORD' , usernameVariable:'USERNAME')]){
     sh 'docker build -t rehamahmed176/my-app1:jmvn-2.0 .'
     sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
     sh 'docker push rehamahmed176/my-app1:jmvn-1.0'
